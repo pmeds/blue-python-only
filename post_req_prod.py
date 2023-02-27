0aba606e035f82a9ba26bc6944b97dc60aa8cd0a
pmeds <109703314+pmeds@users.noreply.github.com>
2023-02-27 16:24:27 -0600

    Update post_req_prod.py

--- post_req_prod.py
+++ post_req_prod.py
@@ -8,33 +8,27 @@ import warnings
 # Check if argument was passed. If no argument was passed then don't do anything.
 # Ideally this scenario should not happen because there is a pre-check on Jenkins to see if the files exists.
 
-
 if len(sys.argv) < 2:
     print('No file provided, no rules to upload')
+    exit()
 else:
     file_name = sys.argv[1]
 
-    if file_name:
-        f = file_name
-        print(f)
-        with open(f, 'r') as file:
-            # Read the CSV file
-            reader = csv.DictReader(file)
-            # Iterate over the rows
-            for row in reader:
-                # Convert the row to a JSON string
-                json_data = json.dumps(row)
-                print(json_data)
-                # Define the POST request's URL
-                url = 'https://paulm-sony.test.edgekey.net/upload'
-                # Define the POST request's headers
-                headers = {"Content-type": "application/json", "Host": "paulm-sony.test.edgekey.net"}
-                # Make the POST request
-                # requests.packages.urllib3.disable_warnings()
-                response = requests.post(url, data=json_data, headers=headers, verify=False)
-                # Print the response
-                rheaders = response.headers
-                rresponse = response.status_code
-                print(rresponse, rheaders)
-    else:
-        print('File not found, no rules to upload')
+if file_name:
+    reader = csv.DictReader(open(file_name))
+    # Iterate over the rows
+    for row in reader:
+        # Convert the row to a JSON string
+        json_data = json.dumps(row)
+        print(json_data)
+        # Define the POST request's URL
+        url = 'https://paulm-sony.test.edgekey.net/upload'
+        # Define the POST request's headers
+        headers = {"Content-type": "application/json", "Host": "paulm-sony.test.edgekey.net"}
+        # Make the POST request
+        requests.packages.urllib3.disable_warnings()
+        response = requests.post(url, data=json_data, headers=headers, verify=False)
+        # Print the response
+        rheaders = response.headers
+        rresponse = response.status_code
+        print(rresponse, rheaders)
